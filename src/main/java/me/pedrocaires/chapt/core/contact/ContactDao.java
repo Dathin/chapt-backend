27cf6efbbbc5846d950d9e79a64e3f62183404a2
pedro.caires <pedro.caires@iteris.com.br>
2021-02-11 11:39:10 -0300

    Fixed connections closing issues

--- src/main/java/me/pedrocaires/chapt/core/contact/ContactDao.java
+++ src/main/java/me/pedrocaires/chapt/core/contact/ContactDao.java
@@ -13,18 +13,18 @@ public class ContactDao {
     private static final String SELECT_CONTACTS = "SELECT U.ID, U.E_MAIL FROM CONTACTS AS C INNER JOIN USERS U on C.CONTACT_ID = U.ID WHERE USER_ID = ?";
 
     public List<ContactResponse> getContactsByUserId(int userId) throws SQLException {
-        var connection = MySqlPool.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CONTACTS);
-        preparedStatement.setInt(1, userId);
-        ResultSet rs = preparedStatement.executeQuery();
-        List<ContactResponse> contactResponses = new ArrayList<>();
-        while (rs.next()) {
-            ContactResponse contactResponse = new ContactResponse();
-            contactResponse.setId(rs.getInt("ID"));
-            contactResponse.seteMail(rs.getString("E_MAIL"));
-            contactResponses.add(contactResponse);
+        try (var connection = MySqlPool.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_CONTACTS);
+            preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
+            List<ContactResponse> contactResponses = new ArrayList<>();
+            while (rs.next()) {
+                ContactResponse contactResponse = new ContactResponse();
+                contactResponse.setId(rs.getInt("ID"));
+                contactResponse.seteMail(rs.getString("E_MAIL"));
+                contactResponses.add(contactResponse);
+            }
+            return contactResponses;
         }
-        connection.close();
-        return contactResponses;
     }
 }
--- src/main/java/me/pedrocaires/chapt/core/message/MessageDao.java
+++ src/main/java/me/pedrocaires/chapt/core/message/MessageDao.java
@@ -12,31 +12,31 @@ public class MessageDao {
     private static final String SELECT_LAST_MESSAGES = "SELECT `FROM`, `TO`, CONTENT FROM MESSAGES WHERE (`FROM` = ? AND `TO` = ?) OR (`FROM` = ? AND `TO` = ?) LIMIT ? OFFSET ?";
 
     public void insert(Integer from, Message message) throws SQLException {
-        var connection = MySqlPool.getConnection();
-        var preparedStatement = connection.prepareStatement(INSERT_MESSAGE);
-        preparedStatement.setInt(1, from);
-        preparedStatement.setInt(2, message.getTo());
-        preparedStatement.setString(3, message.getContent());
-        preparedStatement.execute();
-        connection.close();
+        try (var connection = MySqlPool.getConnection()) {
+            var preparedStatement = connection.prepareStatement(INSERT_MESSAGE);
+            preparedStatement.setInt(1, from);
+            preparedStatement.setInt(2, message.getTo());
+            preparedStatement.setString(3, message.getContent());
+            preparedStatement.execute();
+        }
     }
 
     public List<Message> selectLastMessages(LastMessageRequest lastMessageRequest) throws SQLException {
-        var connection = MySqlPool.getConnection();
-        var preparedStatement = connection.prepareStatement(SELECT_LAST_MESSAGES);
-        preparedStatement.setInt(1, lastMessageRequest.getFrom());
-        preparedStatement.setInt(2, lastMessageRequest.getTo());
-        preparedStatement.setInt(4, lastMessageRequest.getFrom());
-        preparedStatement.setInt(3, lastMessageRequest.getTo());
-        preparedStatement.setInt(5, lastMessageRequest.getPageSize());
-        preparedStatement.setInt(6, calculateOffset(lastMessageRequest.getPageSize(), lastMessageRequest.getPageIndex()));
-        var messages = new ArrayList<Message>();
-        var rs = preparedStatement.executeQuery();
-        while (rs.next()) {
-            messages.add(new Message(rs.getInt("to"), rs.getString("content")));
+        try (var connection = MySqlPool.getConnection()) {
+            var preparedStatement = connection.prepareStatement(SELECT_LAST_MESSAGES);
+            preparedStatement.setInt(1, lastMessageRequest.getFrom());
+            preparedStatement.setInt(2, lastMessageRequest.getTo());
+            preparedStatement.setInt(4, lastMessageRequest.getFrom());
+            preparedStatement.setInt(3, lastMessageRequest.getTo());
+            preparedStatement.setInt(5, lastMessageRequest.getPageSize());
+            preparedStatement.setInt(6, calculateOffset(lastMessageRequest.getPageSize(), lastMessageRequest.getPageIndex()));
+            var messages = new ArrayList<Message>();
+            var rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                messages.add(new Message(rs.getInt("to"), rs.getString("content")));
+            }
+            return messages;
         }
-        connection.close();
-        return messages;
     }
 
     private int calculateOffset(int pageSize, int pageIndex) {
