ef495d095d71c5744b58474232ca61240cd17869
pedro.caires <pedro.caires@iteris.com.br>
2021-02-11 13:25:05 -0300

    Fixed sonarcloud vulnerabilities

--- src/main/java/me/pedrocaires/chapt/core/base/ControllerBase.java
+++ src/main/java/me/pedrocaires/chapt/core/base/ControllerBase.java
@@ -20,52 +20,38 @@ public abstract class ControllerBase extends HttpServlet {
         throw new MethodNotAllowed();
     }
 
-    public <T> T doCustomGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+    public <T> T doCustomGet(HttpServletRequest req, HttpServletResponse resp) throws SQLException {
         throw new MethodNotAllowed();
     }
 
-//    public <T> T doCustomPut(HttpServletRequest req, HttpServletResponse resp) {
-//        throw new MethodNotAllowed();
-//    }
-//
-//    public <T> T doCustomDelete(HttpServletRequest req, HttpServletResponse resp) {
-//        throw new MethodNotAllowed();
-//    }
-
-
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         handleRequest(req, resp, HttpMethod.GET);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         handleRequest(req, resp, HttpMethod.POST);
     }
-
-//    @Override
-//    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        handleRequest(req, resp, HttpMethod.PUT);
-//    }
-//
-//    @Override
-//    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        handleRequest(req, resp, HttpMethod.DELETE);
-//    }
 
     protected int getUserId(HttpServletRequest req) {
         return (int) req.getAttribute(USER_ID);
     }
 
-    private void handleRequest(HttpServletRequest req, HttpServletResponse resp, HttpMethod httpMethod) throws IOException {
+    private void handleRequest(HttpServletRequest req, HttpServletResponse resp, HttpMethod httpMethod) {
         try {
-            writeSuccessObject(getEndpointResponse(req, resp, httpMethod), resp);
-        } catch (ChaptException ex) {
+            try {
+                writeSuccessObject(getEndpointResponse(req, resp, httpMethod), resp);
+            } catch (ChaptException ex) {
+                ex.printStackTrace();
+                catchChaptException(ex, resp);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                catchUnexpectedException(resp);
+            }
+        } catch (IOException ex) {
             ex.printStackTrace();
-            catchChaptException(ex, resp);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            catchUnexpectedException(resp);
+            throw new UnexpectedException();
         }
     }
 
@@ -75,10 +61,6 @@ public abstract class ControllerBase extends HttpServlet {
                 return doCustomGet(req, resp);
             case POST:
                 return doCustomPost(req, resp);
-//            case PUT:
-//                return doCustomPut(req, resp);
-//            case DELETE:
-//                return doCustomDelete(req, resp);
             default:
                 throw new MethodNotAllowed();
         }
